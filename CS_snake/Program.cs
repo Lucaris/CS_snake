3096f428124377ef8b9e572343d522a985221113
Lucaris <kirill.reshetov@gmail.com>
2019-10-05 01:37:44 +0900

    test

--- CS_snake/Program.cs
+++ CS_snake/Program.cs
@@ -6,7 +6,7 @@ namespace CS_snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello There!");
         }
     }
 }
